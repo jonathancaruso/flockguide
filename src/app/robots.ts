f5ada146970c41adf7509ee8b16577d70ec30c22
Jonathan Caruso <jcaruso09@gmail.com>
2026-02-18 21:54:46 -0500

    fix: robots.txt URL to flockguide.com

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://backyardchickens.guide/sitemap.xml',
+    sitemap: 'https://flockguide.com/sitemap.xml',
   }
 }
